057c7d07dfe21e236090a1b94cf3b121f6403606
Jason DeCamp <jason@fourfightingfoxes.com>
2015-12-08 12:45:13 -0500

    refactor parallax positioning to be relative to object centered on the screen in original position. added vertical and horizontal translation options as well as bounce and fade options.

--- scripts/angular-parallax.js
+++ scripts/angular-parallax.js
@@ -1,29 +1,72 @@
 'use strict';
 
 angular.module('angular-parallax', [
-]).directive('parallax', ['$window', function($window) {
+]).directive('parallax', ['$window','$timeout','$parse',
+function($window,$timeout,$parse) {
   return {
     restrict: 'A',
     scope: {
-      parallaxRatio: '@',
-      parallaxVerticalOffset: '@',
-      parallaxHorizontalOffset: '@',
+      parallax: '=',
     },
     link: function($scope, elem, attrs) {
-      var setPosition = function () {
-        if(!$scope.parallaxHorizontalOffset) $scope.parallaxHorizontalOffset = '0';
-        var calcValY = $window.pageYOffset * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1 );
-        if (calcValY <= $window.innerHeight) {
-          var topVal = (calcValY < $scope.parallaxVerticalOffset ? $scope.parallaxVerticalOffset : calcValY);
-          var hozVal = ($scope.parallaxHorizontalOffset.indexOf("%") === -1 ? $scope.parallaxHorizontalOffset + 'px' : $scope.parallaxHorizontalOffset);
-          elem.css('transform', 'translate(' + hozVal + ', ' + topVal + 'px)');
+
+      // set defaults
+      var centerScrollDistance,
+        offsetStart = {
+          vertical:0,
+          horizontal:0
+        },
+        defaults = {
+          vertical: {
+            ratio: 1.1,
+            offset: 0,
+            bounce: false
+          },
+          horizontal: {
+            ratio: 0,
+            offset: 0,
+            bounce: false
+          },
+          fade: false
+        },
+        options = angular.merge({},defaults,$scope.parallax);
+
+      var init = function() {
+        // start with no transform to find original position
+        elem.css('transform','translate(0px, 0px)');
+        centerScrollDistance =
+          elem.offset().top + elem.outerHeight()/2 -$window.innerHeight/2
+        offsetStart = {
+          vertical: centerScrollDistance*options.vertical.ratio,
+          horizontal: centerScrollDistance*options.horizontal.ratio,
         }
+        update();
+      }
+
+      var update = function () {
+        var currentScroll = $window.pageYOffset;
+        var offset = {
+          vertical: currentScroll*options.vertical.ratio + options.vertical.offset - offsetStart.vertical,
+          horizontal: currentScroll*options.horizontal.ratio + options.horizontal.offset - offsetStart.horizontal
+        }
+        if(options.horizontal.bounce && (currentScroll > centerScrollDistance))
+          offset.horizontal = -offset.horizontal;
+        if(options.vertical.bounce && (currentScroll > centerScrollDistance))
+          offset.vertical = -offset.vertical;
+        elem.css('transform','translate('+offset.horizontal+'px, '+offset.vertical+'px)');
+        if(options.fade)
+          elem.css('opacity', 1-Math.min(Math.abs(centerScrollDistance-currentScroll)/($window.innerHeight/2),1));
       };
 
-      setPosition();
+      $timeout(init,0);
+      angular.element($window).bind("resize.parallax", init);
+      angular.element($window).bind("scroll.parallax", update);
+      angular.element($window).bind("touchmove.parallax", update);
 
-      angular.element($window).bind("scroll", setPosition);
-      angular.element($window).bind("touchmove", setPosition);
+      // cleanup
+      $scope.$on('$destroy', function() {
+        document.unbind('.parallax');
+      });
     }  // link function
   };
 }]).directive('parallaxBackground', ['$window', function($window) {
@@ -33,23 +76,28 @@ angular.module('angular-parallax', [
     template: '<div ng-transclude></div>',
     scope: {
       parallaxRatio: '@',
-      parallaxVerticalOffset: '@',
     },
     link: function($scope, elem, attrs) {
       var setPosition = function () {
-        var calcValY = (elem.prop('offsetTop') - $window.pageYOffset) * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1) - ($scope.parallaxVerticalOffset || 0);
+        var calcValY = (elem.prop('offsetTop') - $window.pageYOffset) * ($scope.parallaxRatio ? $scope.parallaxRatio : 1.1 );
         // horizontal positioning
         elem.css('background-position', "50% " + calcValY + "px");
       };
 
       // set our initial position - fixes webkit background render bug
-      angular.element($window).bind('load', function(e) {
+      angular.element($window).bind('load.parallax', function(e) {
         setPosition();
         $scope.$apply();
       });
 
-      angular.element($window).bind("scroll", setPosition);
-      angular.element($window).bind("touchmove", setPosition);
+      angular.element($window).bind("scroll.parallax", setPosition);
+      angular.element($window).bind("touchmove.parallax", setPosition);
+
+      // cleanup
+      $scope.$on('$destroy', function() {
+        document.unbind('.parallax');
+      });
+
     }  // link function
   };
 }]);
